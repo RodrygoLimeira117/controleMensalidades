3adb5d8f860e27c4f15e611abadfa519eed6a30c
Rodrigo Limeira <rodrigolimeira2002@outlook.com>
2026-07-20 21:41:53 -0300

    Delete muaythai-api directory

--- muaythai-api/src/main/java/com/rodrigolimeira/tatamemanager/repository/ConfiguracaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rodrigolimeira.tatamemanager.repository;
-
-import com.rodrigolimeira.tatamemanager.model.Configuracao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfiguracaoRepository extends JpaRepository<Configuracao, Long> {
-}
