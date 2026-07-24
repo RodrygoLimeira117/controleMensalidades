0f1d2c07a0c048dbc54915b0a17a735be2a42e90
Rodrigo Limeira <rodrigolimeira2002@outlook.com>
2026-07-23 23:35:11 -0300

    Delete muaythai-api directory

--- muaythai-api/src/main/java/com/rodrigolimeira/tatamemanager/controller/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.rodrigolimeira.tatamemanager.controller;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(
-        @NotBlank(message = "Informe o usuário.") String username,
-        @NotBlank(message = "Informe a senha.") String password
-) {
-}
